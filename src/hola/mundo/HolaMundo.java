f82475bc8fd1328b1d6c05538ed5459077c5ffa0
Estudiante <Estudiante@BE454S403C02ING.unal.loc>
2019-04-02 08:46:11 -0500

    ejemplo hola mundo

--- src/hola/mundo/HolaMundo.java
+++ src/hola/mundo/HolaMundo.java
@@ -1,11 +1,15 @@
 
-package hola.mundo;
 
+package hola.mundo;
+import java.util.*;
 public class HolaMundo {
 
     
     public static void main(String[] arg) {
-        System.out.println("hola mundo");
+     Scanner lectura = new Scanner (System.in);
+        System.out.println("ingrese un numero ");
+        double x = lectura.nextDouble();
+        System.out.println("ingreso "+x);
         
     }
     
